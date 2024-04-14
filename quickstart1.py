3ddef1bf1aabaaac1ca0e7fd55e66ff35110850c
JBC-developer <166973604+JBC-developer@users.noreply.github.com>
2024-04-14 15:25:21 +0530

    Update quickstart1.py

--- quickstart1.py
+++ quickstart1.py
@@ -40,7 +40,8 @@ def main(dicts : dict, item_range : str):
       creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
     with open("/etc/secrets/token.json", "w") as token:
-      token.write(creds.to_json())
+      print()
+      #token.write(creds.to_json())
 
   try:
     service = build("sheets", "v4", credentials=creds)
