3b928a5b92f21c6647c7421b083a0aa9a3589b0f
JBC-developer <166973604+JBC-developer@users.noreply.github.com>
2024-04-15 20:37:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,10 @@ async def on_ready():
 
     for guild in bot.guilds:
         if guild.id not in list(channel_dict.keys()):
-            channel = guild.text_channels[0]
-            await channel.send('The bot has not been set up. Please use /setup and select the channel for it to work in')
+            for channel in guild.text_channels:
+                if channel.permissions_for(guild.me).send_messages:
+                    await channel.send('The bot has not been set up. Please use /setup and select the channel for it to work in')
+                break
 
 
 
@@ -183,7 +185,9 @@ async def on_message(message):
 
 @bot.event
 async def on_guild_join(guild):
-    channel = guild.text_channels[0]
-    await channel.send("Hello! Thank you for using JB Value Helper!\nIt uses JBC's value list | discord.gg/jbc\nPlease set the bot up by using /setup and select the channel you want the bot to be used in.\nUse /help for the list of commands")
+    for channel in guild.text_channels:
+        if channel.permissions_for(guild.me).send_messages:
+            await channel.send("Hello! Thank you for using JB Value Helper!\nIt uses JBC's value list | discord.gg/jbc\nPlease set the bot up by using /setup and select the channel you want the bot to be used in.\nUse /help for the list of commands")
+        break
 
 bot.run(Token.TOKEN)
