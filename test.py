defb2b5afae5b305e1747b05f7473f8142b56d5f
JBC-developer <166973604+JBC-developer@users.noreply.github.com>
2024-05-08 13:32:33 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
+
 import numpy as np
 import quickstart1
+import values
 
 def main(images : dict):
     values1 : dict
@@ -8,14 +10,16 @@ def main(images : dict):
     v = list(values1.values())
     values2 = {}
 
-    values2 = quickstart1.main(values2,"Textures!C9:K200")
+    #values2 = quickstart1.main(values2,"Textures!C9:K200")
     values2 = quickstart1.main(values2,'Hyperchromes!C10:K200')
-    values2 = quickstart1.main(values2,"Colors!C9:K200")
-    values2 = quickstart1.main(values2,"Rims!C9:K200")
-    values2 = quickstart1.main(values2,"Spoilers!C9:K200")
-    values2 = quickstart1.main(values2,"Tires & Horns!C9:M200")
-    values2 = quickstart1.main(values2,"Furniture!C9:K200")
-    values2 = quickstart1.main(values2,"Vehicles!C9:K200")
+   # values2 = quickstart1.main(values2,"Colors!C9:K200")
+   # values2 = quickstart1.main(values2,"Rims!C9:K200")
+   # values2 = quickstart1.main(values2,"Spoilers!C9:K200")
+   # values2 = quickstart1.main(values2,"Tires & Horns!C9:M200")
+   # values2 = quickstart1.main(values2,"Furniture!C9:K200")
+    values2 = quickstart1.main(values2,"Vehicles!C61:K200")
+    values2 = values.main(values2, 'Value List!C20:F400')
+    values2 = values.main(values2, 'Hyperchromes!C22:E63')
 
     
 
@@ -26,15 +30,18 @@ def main(images : dict):
     values2 = {k: v for k, v in zip(k,v)}
 
     #print(images)
-
+    values1 = {}
+    for key in values2.keys():
+        values1[key.strip().lower()] = values2[key]
     for ka in k:
         if ka.lower() in list(images.keys()):
-            values2[ka][0] = images[ka.lower()]
+            values1[ka.lower()][0] = images[ka.lower()]
         if ka in list(images.keys()):
-            values2[ka][0] = images[ka]
+            values1[ka.lower()][0] = images[ka]
     
-    #for key, value in values2.items() :
-        #print (("'"+ key +"'").strip(), ':', value, end=',\n')
+    for key, value in values1.items() :
+        if value[0] == '':
+            print (("'"+ key +"'").strip(), ':', value, end=',\n')
+    values1['torpedo'][0] = 'https://static.wikia.nocookie.net/rblx-jailbreak/images/4/40/Torpedo.png/revision/latest/scale-to-width-down/1000?cb=20220219030137.png'
 
-    np.save('ValueList.npy', values2)
-    
+    np.save('ValueList.npy', values1)
